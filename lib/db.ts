6b0f60e5c2426c7bf3954184ff2c31dd615cf807
Kuzuri247 <mindsetmatters47@gmail.com>
2025-02-01 20:12:07 +0530

    added formatter

--- lib/db.ts
+++ lib/db.ts
@@ -11,32 +11,30 @@ if (!MONGODB_URI) {
   throw new Error("Please define mongodb uri in env file");
 }
 
-
 let cached = global.mongoose;
-if(!cached){
+if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
 }
-export async function connecttoDatabase(){
-    if (cached.conn) {
-        return cached.conn;
-    }
+export async function connecttoDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
-    if(!cached.promise){
-    const opts ={
-        bufferCommands: true,
-        maxPoolSize: 10,
-    }
-    
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+
     cached.promise = mongoose
-    .connect(MONGODB_URI,opts)
-    .then(() => mongoose.connection);
-}
-   try{
-        cached.conn = await cached.promise;        
-   }
-   catch(error){
+      .connect(MONGODB_URI, opts)
+      .then(() => mongoose.connection);
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
     cached.promise = null;
     throw new Error("Failed to connect to the database");
-   }
-   return cached.conn;
-}   
+  }
+  return cached.conn;
+}
